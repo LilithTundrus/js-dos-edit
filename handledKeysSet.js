5e243433da67256b50e8cdad3868989a2409d1b2
Lilith Tundrus <echofox123@gmail.com>
2018-08-01 18:38:10 -0400

    Added a Set of keynames that will be handled by other code, this file will be used within the main script

---
+++ handledKeysSet.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Set of keyname strings for checking against within the main keypress event
+// Any keys that need custom code should go here
+
+let customKeys = new Set();
+customKeys.add('pageup');
+customKeys.add('pagedown');
+customKeys.add('left');
+customKeys.add('right');
+customKeys.add('up');
+customKeys.add('down');
+customKeys.add('enter');
+customKeys.add('backspace');
+customKeys.add('tab');
+customKeys.add('space');
+
+// Export the set to be used by the main script/wherever else it's needed
+module.exports = customKeys;
