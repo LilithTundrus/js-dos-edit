80b7ec611e06ac9bf97554a72a4599ff39c15569
Lilith Tundrus <echofox123@gmail.com>
2018-08-22 19:14:00 -0400

    Added the todo list from editor.js

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
+// Using ES5 strict mode
 'use strict';
-// Main entry point for the editor, checks for command line arguments and 
+
+// This is the main entry point for the editor, checks for command line arguments and 
 // starts up the editor in a state where a file is already being edited
+
+// NOTE: the M denotes meta (alt key) for key listeners
+// NOTE: Alt codes like ↑ work in blessed!
+// NOTE: The version of blessed we're using is modified, the keys.js file has a regex to 
+// ignore a couple extra things (mainly cursor reporting bullshit that's just, not handled by blessed.)
+// NOTE: blessed is weird, if you make a change and it doesn't happen, try rendering in between each of
+// the steps, they may not occur otherwise
+
+// TODO: Add more info to the statusBar area (if we can get the cursor to stop moving when it updates)
+// TODO: Document keyhandlers better
+// TODO: Fix this bug:
+// TypeError: this._clines.rtof is not a function
+// TODO: Get custom horizontal scrolling working
+// TODO: Add half-width shadows for buttons
+// TODO: Get the scroll arrows to 'blink' on arrow key events (works but annoyingly moves the cursor around to make the change)
+// TODO: Better figure out DOS edit's behaviour to better match it
+// TODO: Handle resizing a bit better at some point
+// TODO: Get saving/opening documents working as well as checking if the currently edited document is saved or not
+// TODO: Redo how cursor reflowing on the up/down arrow works (more like vim/vscode)
+// TODO: This will require a lot of polish that's going to take a lot of time to work through
+// TODO: Standardize blessed component geneeration order of options for components!
+// TODO: FIX THER ORGANIZATION OF THIS PROJECT AAAAA
+
+/* Current working list:
+
+Right now I think the main idea is that before working on the rest of the text editor, the 
+actual text editing needs to be addressed. So I'll make sure that's perfect first
+
+First basic editing controls, - DONE (sort of) -- have bugs to iron out
+then scrolling (and scrollbars), - DONE (mostly) -- still a bit of weirdness that needs to be worked through
+then opening/reading files, - WORKING ON -- need the logic to check on the status of files + saving to user-defined files
+then menus,
+then horizontal scrolling,
+error handling,
+user-facing error handling
+then the rest
+*/
 
 // Node/NPM package requires
 const fs = require('fs');
