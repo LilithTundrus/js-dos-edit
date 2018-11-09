33f244d7a0050df57146458cc22366d4e45c8efb
Lilith Tundrus <echofox123@gmail.com>
2018-11-09 15:18:14 -0500

    Added a file to create a horizontal scrollbar for

---
+++ ui-components/horizontalScrollBar.js
@@ -0,0 +1,12 @@
+// Using ES5 strict mode
+'use strict';
+
+// Node/NPM package requires
+const fs = require('fs');
+const blessed = require('neo-blessed');
+
+// This file contains one of the blessed components for constructing the UI in an effort to
+// keep this project modular
+
+// Create the horizontal scrollbar for the edtior, this will update when the current line goes beyond
+// the standard 80-ish column length
