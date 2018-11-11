a3049f19f9d8a7650ed16b1426f09243f7cdfa1a
Lilith Tundrus <echofox123@gmail.com>
2018-11-11 01:42:13 -0500

    Starting to initialize a horizontal scrollbar class

--- ui-components/horizontalScrollBar.js
+++ ui-components/horizontalScrollBar.js
@@ -10,3 +10,39 @@ const blessed = require('neo-blessed');
 
 // Create the horizontal scrollbar for the edtior, this will update when the current line goes beyond
 // the standard 80-ish column length
+
+
+class HorizontalScrollBar {
+
+    /** Creates an instance of HorizontalScrollBar. This is the main text entry box
+     * @param {*} parent Blessed parent parent to attach the element to
+     * @memberof HorizontalScrollBar
+     */
+    constructor(parent) {
+        this.parent = parent;
+
+        this.horizontalScrollBar = blessed.box({
+            parent: this.parent,
+
+            // Component relative position options
+
+            // Place the scrollbar at the BOTTOM of the screen + 1 to be above the status bar
+            bottom: 'bottom' - 4,
+
+            // Component size options
+            // Scrollbar should occupy 100% of the screen horizontally
+            width: '100%',
+            height: 1,
+
+            style: {
+                fg: 'black',
+                bg: 'light-grey',
+            },
+            content: 'aaaaaaaaaaa'
+        })
+    }
+
+}
+
+// Export the blessed component
+module.exports = HorizontalScrollBar;
